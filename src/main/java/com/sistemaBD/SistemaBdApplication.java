887f1ecd0261d8ac41fa63c5332b7b7997d9e213
alexanderCanon <alexandercanon64@gmail.com>
2025-09-30 20:55:21 -0600

    Get out

--- src/main/java/com/sistemaBD/SistemaBdApplication.java
+++ src/main/java/com/sistemaBD/SistemaBdApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaBdApplication {
 
+    /**
+     * Hi everyone, this is our DB project
+     * @param args
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaBdApplication.class, args);
 	}
+
 
 }
